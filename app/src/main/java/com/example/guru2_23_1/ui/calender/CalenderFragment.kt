45d6b9cabf44529854e417bf7f5e6701858a8e14
millie052430@gmail.com <millie052430@gmail.com>
2023-07-24 18:35:03 +0900

    CalenderFragment

---
+++ app/src/main/java/com/example/guru2_23_1/ui/calender/CalenderFragment.kt
@@ -0,0 +1,67 @@
+package com.example.guru2_23_1.ui.calender
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.guru2_23_1.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+
+class CalenderFragment : Fragment() {
+
+    private lateinit var adapter : MemoAdapter
+    private var selectday = "";
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val root: View = inflater.inflate(R.layout.fragment_calender, container, false)
+        val textView: TextView = root.findViewById(R.id.text_calender)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val calendarView = view.findViewById<CalendarView>(R.id.calendar_view)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = MemoAdapter(reqireContext())
+        recyclerView.adapter = adapter
+
+        calendarView.setOnDateChangeListener(object : CalendarView.onDateChangeListener{
+            override fun onSelectedDayChange(p0: calendarView, p1: Int, p2: Int, p3: Int){
+                if(selectday.equals(String.format("%04d%02d%02d",p1, p2+1, 3))){
+                    startActivity(Intent(requireContext(), MemoActivity::class.java))
+                }
+                else{
+                    val calendar : Calendar = Calendar.getInstance()
+                    calendar.set(p1, p2, p3)
+                    val day: String = calendar.timeInMillis.toString().substring(0, 6)
+                    adapter.setList(DBLoader(requireContext()).memoList(day.toLong()))
+                    selectday = p0.toString() + p1.toString()+ p3.toString()
+                }
+            }
+        })
+        val date = Date()
+        date.time = calendarView.date
+        selectday = SimpleDateFormat("yyyyMMdd").format(date)
+        adapter.setList(
+            DBLoader(requireContext()).memoList(
+            calendarView.date.toString().substring(0, 6).toLong()
+            )
+        )
+
+    }
+}
--- app/src/main/java/com/example/guru2_23_1/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/guru2_23_1/ui/notifications/NotificationsFragment.kt
@@ -7,36 +7,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.guru2_23_1.R
 import com.example.guru2_23_1.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
-
-    private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val root: View = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
         return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
     }
 }
