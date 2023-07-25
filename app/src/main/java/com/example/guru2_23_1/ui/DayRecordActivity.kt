0cc5fdcf775f257c373f9eb01e40c0ece5be6d87
eunji <ejj9704@naver.com>
2023-07-26 03:45:55 +0900

    2023_07_25_EJ_layout_upload

--- app/src/main/java/com/example/guru2_23_1/ui/DayRecordActivity.kt
+++ app/src/main/java/com/example/guru2_23_1/ui/DayRecordActivity.kt
@@ -1,14 +1,34 @@
 package com.example.guru2_23_1.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.guru2_23_1.R
 
 class DayRecordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_day_record)
+
+        fun onMealClick(v: View) {
+            val intent = Intent(this, MealActivity::class.java)
+            startActivity(intent)
+        }
+
+//        fun onScheduleClick(v: View) {
+//            val intent = Intent(this, MealActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        fun onMoodClick(v: View) {
+            val intent = Intent(this, DiaryActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
 
 //    fun onLunchClick(view: View) {
 //        // 식사 페이지로 전환
