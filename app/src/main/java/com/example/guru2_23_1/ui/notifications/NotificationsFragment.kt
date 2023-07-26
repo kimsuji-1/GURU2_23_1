b5b479fd4cc62e123415e620a7d22bf881f9fcf7
millie052430@gmail.com <millie052430@gmail.com>
2023-07-26 23:02:21 +0900

    액티비티 간 전환

--- app/src/main/java/com/example/guru2_23_1/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/guru2_23_1/ui/notifications/NotificationsFragment.kt
@@ -5,21 +5,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.guru2_23_1.R
+import com.example.guru2_23_1.databinding.FragmentCalenderBinding
 import com.example.guru2_23_1.databinding.FragmentNotificationsBinding
+import com.example.guru2_23_1.ui.DiaryActivity
+import com.example.guru2_23_1.ui.MealActivity
+import com.example.guru2_23_1.ui.SchelduleActivity
 
 class NotificationsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val root: View = inflater.inflate(R.layout.fragment_notifications, container, false)
-        return root
 
+    private var _binding: FragmentNotificationsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonMeal.setOnClickListener {startActivity(Intent(requireContext(), MealActivity::class.java))}
+        binding.buttonTodo.setOnClickListener {startActivity(Intent(requireContext(), SchelduleActivity::class.java))}
+        binding.buttonMood.setOnClickListener {startActivity(Intent(requireContext(), DiaryActivity::class.java))}
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
