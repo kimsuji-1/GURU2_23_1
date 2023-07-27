c2c618e8405cbb29cc56c42027bf968a1e1527de
kimsuji <mymilk123@naver.com>
2023-07-27 14:15:12 +0900

    캘린더 버튼 인식 오류

---
+++ app/src/main/java/com/example/guru2_23_1/ui/CalendarActivity.kt
@@ -0,0 +1,32 @@
+package com.example.guru2_23_1.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.guru2_23_1.R
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+
+        val buttontoday = findViewById<Button>(R.id.today_btn)
+        buttontoday.setOnClickListener {
+            val intent = Intent(this, MonthActivity::class.java)
+            startActivity(intent)
+        }
+
+//        val buttonTodo = findViewById<Button>(R.id.month_year_tv)
+//        buttonTodo.setOnClickListener {
+//            val intent = Intent(this, DayRecordActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        val buttonadd = findViewById<Button>(R.id.add_btn)
+        buttonadd.setOnClickListener {
+            val intent = Intent(this, addActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/guru2_23_1/ui/calender/CalenderFragment.kt
+++ app/src/main/java/com/example/guru2_23_1/ui/calender/CalenderFragment.kt
@@ -1,74 +1,42 @@
 package com.example.guru2_23_1.ui.calender
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.guru2_23_1.R
-import java.text.SimpleDateFormat
-import java.util.*
+import com.example.guru2_23_1.databinding.FragmentCalenderBinding
+import com.example.guru2_23_1.ui.MonthActivity
+import com.example.guru2_23_1.ui.addActivity
 
-class CalendarFragment : Fragment() {
-    val itemList = arrayListOf<Date>()
-    val listAdapter = CalendarAdapter(itemList)
-    lateinit var calendarList: RecyclerView
-    lateinit var mLayoutManager: LinearLayoutManager
-    lateinit var monthYearTv: TextView
+class CalenderFragment : Fragment() {
+
+    private var _binding: FragmentCalenderBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val view = inflater.inflate(R.layout.fragment_calender, container, false)
-        calendarList = view.findViewById(R.id.calendar_list)
-        mLayoutManager = LinearLayoutManager(view.context)
-        monthYearTv = view.findViewById(R.id.month_year_tv)
-
-        // recyclerView orientation (가로 방향 스크롤 설정)
-        mLayoutManager.orientation = LinearLayoutManager.HORIZONTAL
-        calendarList.layoutManager = mLayoutManager
-
-        setListView()
-
-        // 오늘 버튼 클릭 시 CalendarMonthFragment로 전환
-        val todayBtn: Button = view.findViewById(R.id.today_btn)
-        todayBtn.setOnClickListener {
-            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
-            val fragmentTransaction = fragmentManager.beginTransaction()
-            val calendarMonthFragment = CalendarMonthFragment()
-
-            fragmentTransaction.replace(R.id.fragment_container, calendarMonthFragment)
-            fragmentTransaction.addToBackStack(null)
-            fragmentTransaction.commit()
-        }
-
-        return view
+        _binding = FragmentCalenderBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    // list(날짜, 요일)를 만들고, adapter를 등록하는 메소드
-    private fun setListView() {
-        val calendar = Calendar.getInstance()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        // 현재 달의 마지막 날짜
-        val lastDayOfMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
-        val currentMonth = calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US)
-        val currentYear = calendar.get(Calendar.YEAR)
+        binding.buttontoday.setOnClickListener {startActivity(Intent(requireContext(), MonthActivity::class.java))}
+        binding.buttonadd.setOnClickListener {startActivity(Intent(requireContext(), addActivity::class.java))}
+    }
 
-        for (i: Int in 1..lastDayOfMonth) {
-            calendar.set(currentYear, calendar.get(Calendar.MONTH), i)
-            val dayOfWeek = calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US)
-            itemList.add(Date(dayOfWeek, i.toString()))
-        }
-        calendarList.adapter = listAdapter
-
-        // 월과 연도를 표시하는 TextView 갱신
-        monthYearTv.text = "$currentMonth $currentYear"
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
