444c7541c1b2452a92d2c377934c2646e01bdb9f
heoseoseowon <heoseowon@swu.ac.kr>
2023-07-30 01:53:09 +0900

    DBWEATHER 생성

---
+++ app/src/main/java/com/example/guru2_23_1/ui/DB/DBWEATHER.kt
@@ -0,0 +1,22 @@
+package com.example.guru2_23_1.ui.DB
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBWEATHER (context: Context,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+    ): SQLiteOpenHelper(context, "DBWEATHER", null, 1){
+        override fun onCreate(db: SQLiteDatabase?) {
+            db!!.execSQL(
+                "CREATE TABLE DBWEATHER (WH_REGION CHAR(20), DATE date, WEATHER CHAR(20))")
+        }
+
+        override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+            db!!.execSQL("DROP TABLE IF EXISTS DBWEATHER")
+            onCreate(db)
+        }
+
+    }
