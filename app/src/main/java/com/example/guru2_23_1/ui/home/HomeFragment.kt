4b6dfdc76abc223fef4cba5f5e8e14a3122bb836
millie052430@gmail.com <millie052430@gmail.com>
2023-08-01 04:23:39 +0900

    HomeFragment 수정

--- app/src/main/java/com/example/guru2_23_1/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/guru2_23_1/ui/home/HomeFragment.kt
@@ -11,14 +11,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.guru2_23_1.R
 import com.example.guru2_23_1.databinding.FragmentHomeBinding
+import com.example.guru2_23_1.databinding.FragmentNotificationsBinding
 import com.example.guru2_23_1.ui.LoginActivity
 import com.example.guru2_23_1.ui.MealActivity
 import com.example.guru2_23_1.ui.RegisterActivity
+import java.sql.Types.NULL
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
+    val login_check = arguments?.getInt("login_check")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +31,23 @@ class HomeFragment : Fragment() {
         //액션바 배경색 흰 색으로
         (activity as AppCompatActivity).supportActionBar?.setBackgroundDrawable(resources.getDrawable(R.color.white))
         (activity as AppCompatActivity).supportActionBar?.elevation = 0F // 액션바 그림자 안 보이개
+    }
 
-        val login_check = arguments?.getInt("login_check")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+        }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (login_check == NULL){
+            login_check == 0
+        }
 
         if (login_check== 1 ){//로그인 O
             //로그인/회원가입 버튼 안 보이게
@@ -42,15 +60,6 @@ class HomeFragment : Fragment() {
             //홈 멘트 안 보이게
             binding.txtHomemention.visibility = View.GONE
         }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val root: View = inflater.inflate(R.layout.fragment_home, container, false)
-        return root
 
         binding.btnLogin.setOnClickListener {startActivity(Intent(requireContext(), LoginActivity::class.java))}
         binding.btnRegister.setOnClickListener {startActivity(Intent(requireContext(), RegisterActivity::class.java))}
